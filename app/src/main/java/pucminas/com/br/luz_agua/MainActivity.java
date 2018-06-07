2551cb7e360a55d7ab6601bf3e9cb1e08a696918
GeovaneF55 <geovanefss@gmail.com>
2018-06-07 17:10:17 -0300

    Labels account register

--- app/src/main/java/pucminas/com/br/luz_agua/MainActivity.java
+++ app/src/main/java/pucminas/com/br/luz_agua/MainActivity.java
@@ -1,7 +1,6 @@
 package pucminas.com.br.luz_agua;
 
 import android.content.Intent;
-import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.NavigationView;
@@ -97,16 +96,12 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
