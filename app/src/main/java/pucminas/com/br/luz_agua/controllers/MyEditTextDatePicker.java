b49a9b3c5f8889bfa993c247f895cb0f3344635e
GeovaneF55 <geovanefss@gmail.com>
2018-06-08 16:37:20 -0300

    Account Register Fields Complete

---
+++ app/src/main/java/pucminas/com/br/luz_agua/controllers/MyEditTextDatePicker.java
@@ -0,0 +1,67 @@
+package pucminas.com.br.luz_agua.controllers;
+
+import android.app.Activity;
+import android.app.DatePickerDialog;
+import android.content.Context;
+import android.view.View;
+import android.widget.DatePicker;
+import android.widget.EditText;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.TimeZone;
+
+public class MyEditTextDatePicker implements View.OnClickListener, DatePickerDialog.OnDateSetListener {
+    private EditText _editText;
+    private int _day;
+    private int _month;
+    private int _year;
+    private Context _context;
+
+    public MyEditTextDatePicker(Context context, int editTextViewID)
+    {
+        Activity act = (Activity)context;
+        this._editText = (EditText)act.findViewById(editTextViewID);
+        this._editText.setOnClickListener(this);
+        this._context = context;
+    }
+
+    @Override
+    public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+        _year = year;
+        _month = monthOfYear;
+        _day = dayOfMonth;
+        try {
+            updateDisplay();
+        } catch (ParseException e) {
+            this._editText.setError(null);
+        }
+    }
+
+    @Override
+    public void onClick(View v) {
+        Calendar calendar = Calendar.getInstance(TimeZone.getDefault());
+
+        DatePickerDialog dialog = new DatePickerDialog(_context, this,
+                calendar.get(Calendar.YEAR), calendar.get(Calendar.MONTH),
+                calendar.get(Calendar.DAY_OF_MONTH));
+        dialog.show();
+
+    }
+
+    // updates the date in the birth date EditText
+    private void updateDisplay() throws ParseException {
+
+        SimpleDateFormat formatador = new SimpleDateFormat("dd/MM/yyyy");
+        StringBuilder stringBuilder = new StringBuilder().append(_day).append("/").append(_month + 1).append("/").append(_year).append(" ");
+        Date data = formatador.parse(stringBuilder.toString());
+
+        this._editText.setText(formatador.format(data));
+        this._editText.setText(new StringBuilder()
+                // Month is 0 based so add 1
+                .append(_day).append("/").append(_month + 1).append("/").append(_year).append(" "));
+        this._editText.setError(null);
+    }
+}
--- app/src/main/java/pucminas/com/br/luz_agua/fragments/AccountRegister.java
+++ app/src/main/java/pucminas/com/br/luz_agua/fragments/AccountRegister.java
@@ -3,14 +3,22 @@ package pucminas.com.br.luz_agua.fragments;
 import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+import java.util.Objects;
 
 import pucminas.com.br.luz_agua.R;
+import pucminas.com.br.luz_agua.controllers.MyEditTextDatePicker;
 
-public class AccountRegister extends Fragment {
+public class AccountRegister extends Fragment{
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
@@ -45,6 +53,7 @@ public class AccountRegister extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         if (getArguments() != null) {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
@@ -52,10 +61,21 @@ public class AccountRegister extends Fragment {
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+    public void onStart() {
+        super.onStart();
+
+        new MyEditTextDatePicker(this.getActivity() , R.id.input_data_conta);
+
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_account_register, container, false);
+        View view = inflater.inflate(R.layout.fragment_account_register, container, false);
+
+        createComponets(view);
+
+        return view;
     }
 
     // TODO: Rename method, update argument and hook method into UI event
@@ -71,5 +91,34 @@ public class AccountRegister extends Fragment {
     @Override
     public void onDetach() {
         super.onDetach();
+    }
+
+
+    public void createComponets(View view){
+        // Spinner Tipo Pessoa
+        Spinner spinner = (Spinner) view.findViewById(R.id.input_tipo_conta);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(Objects.requireNonNull(this.getActivity()),
+                R.array.spinner_tipo_conta, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_dropdown_item_1line);
+        spinner.setAdapter(adapter);
+
+        // Spinner Tipo Pessoa
+        spinner = (Spinner) view.findViewById(R.id.input_tipo_pessoa);
+        adapter = ArrayAdapter.createFromResource(Objects.requireNonNull(this.getActivity()),
+                R.array.spinner_tipo_pessoa, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_dropdown_item_1line);
+        spinner.setAdapter(adapter);
+
+        FloatingActionButton fab = view.findViewById(R.id.new_conta);
+        fab.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Geovane lindo!!", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+
+            }
+
+        });
     }
 }
