3e08d38ef8462d4c532f4c24385ed9d190131580
gabriel <gomeslucianogabriel@gmail.com>
2018-06-10 12:24:38 -0300

    gabriel - RecyclerView com cardView

--- app/src/main/java/pucminas/com/br/luz_agua/adapters/ListAdapter.java
+++ app/src/main/java/pucminas/com/br/luz_agua/adapters/ListAdapter.java
@@ -1,35 +1,52 @@
 package pucminas.com.br.luz_agua.adapters;
 
+import android.content.Context;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
+
+import java.util.List;
 
 import pucminas.com.br.luz_agua.R;
 import pucminas.com.br.luz_agua.model.OurData;
 
-public class ListAdapter extends RecyclerView.Adapter {
+public class ListAdapter extends RecyclerView.Adapter<ListAdapter.ListViewHolder> {
+
+    private Context mCtx;
+    private List<OurData> dataList;
+
+    public ListAdapter(Context mCtx, List<OurData> dataList) {
+        this.mCtx = mCtx;
+        this.dataList = dataList;
+    }
+
     @NonNull
     @Override
-    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.list_item, parent,false);
+    public ListViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        LayoutInflater inflater = LayoutInflater.from(mCtx);
+        View view = inflater.inflate(R.layout.list_item, null );
         return new ListViewHolder(view);
     }
 
     @Override
-    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
-        ((ListViewHolder) holder).bindView(position);
+    public void onBindViewHolder(@NonNull ListViewHolder holder, int position) {
+        OurData data = dataList.get(position);
+        holder.titularTextView.setText(data.getNome());
+        holder.cpfTextView.setText(data.getCpf());
 
     }
 
     @Override
     public int getItemCount() {
-        return OurData.titulares.length;
+        return dataList.size();
     }
 
-    private class ListViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+    class ListViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+
         private TextView titularTextView;
         private TextView cpfTextView;
 
@@ -40,16 +57,7 @@ public class ListAdapter extends RecyclerView.Adapter {
             itemView.setOnClickListener(this);
         }
 
-        /**
-         * Adiciona itens do banco à RecyclerView
-         */
-        public void bindView(int posicao) {
-            titularTextView.setText(OurData.titulares[posicao]);
-            cpfTextView.setText(OurData.cpfs[posicao]);
-        }
-
         public void onClick(View view){
-
         }
 
     }
